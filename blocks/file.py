60ada4fa8817f975f0b5bddc0aa4ee33c0cdee35
Sol Kim <defreturnnone@gmail.com>
2018-02-03 14:19:36 +0900

    add ListDir block

--- blocks/file.py
+++ blocks/file.py
@@ -53,3 +53,34 @@ class FileWrite(block.Block):
         with open(path, "w") as f:
             for d in datas:
                 f.write(d)
+
+
+class ListDir(block.Block):
+    def __init__(self):
+        super(ListDir, self).__init__()
+
+    def initialize(self):
+        self.addInput(str, "dirPath")
+        self.addOutput(str, "file")
+        self.addOutput(str, "directory")
+
+    def process(self):
+        in_p = self.input("dirPath").receive()
+
+        if in_p.isEOP():
+            return False
+
+        path = in_p.value()
+        in_p.drop()
+
+        if os.path.isdir(path):
+            for p in os.listdir(path):
+                fp = os.path.abspath(os.path.join(path, p)).replace("\\", "/")
+
+                if os.path.isdir(fp):
+                    self.output("directory").send(fp)
+                    continue
+
+                self.output("file").send(fp)
+
+        return True
