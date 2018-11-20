6f469e3a51c34de985ebabfc0df681653165048b
kim <kim@marza.com>
2018-11-20 18:09:23 +0900

    add das blocks

---
+++ blocks/dasBlocks.py
@@ -0,0 +1,189 @@
+from petitBloc import block
+from petitBloc import anytype
+import das
+import das.cli
+
+
+class DasRead(block.Block):
+    def __init__(self):
+        super(DasRead, self).__init__()
+
+    def initialize(self):
+        self.addInput(str, "filepath")
+        self.addOutput(das.types.TypeBase, "dasObj")
+
+    def process(self):
+        fp = self.input("filepath").receive()
+        if fp.isEOP():
+            return False
+
+        fv = fp.value()
+        fp.drop()
+
+        das_obj = das.read(fv)
+        self.output("dasObj").send(das_obj)
+
+        return True
+
+
+class DasWrite(block.Block):
+    def __init__(self):
+        super(DasWrite, self).__init__()
+
+    def initialize(self):
+        self.addInput(das.types.TypeBase, "dasObj")
+        self.addInput(str, "filepath")
+
+    def process(self):
+        fp = self.input("filepath").receive()
+        if fp.isEOP():
+            return False
+
+        fv = fp.value()
+        fp.drop()
+
+        dp = self.input("dasObj").receive()
+        if dp.isEOP():
+            return False
+
+        dv = dp.value()
+        dp.drop()
+
+        das.write(dv, fv)
+
+        return True
+
+
+class DasGet(block.Block):
+    def __init__(self):
+        super(DasGet, self).__init__()
+
+    def initialize(self):
+        self.addInput(das.types.TypeBase, "dasObj")
+        self.addInput(str, "key")
+        self.addOutput(anytype.AnyType, "result")
+
+    def process(self):
+        dp = self.input("dasObj").receive()
+        if dp.isEOP():
+            return False
+
+        dv = dp.value()
+        dp.drop()
+
+        kp = self.input("key").receive()
+        if kp.isEOP():
+            return False
+
+        kv = kp.value()
+        kp.drop()
+
+        self.output("result").send(das.cli.get(dv, kv))
+
+        return True
+
+
+class DasSet(block.Block):
+    def __init__(self):
+        super(DasSet, self).__init__()
+
+    def initialize(self):
+        self.addInput(das.types.TypeBase, "inDasObj")
+        self.addInput(str, "key")
+        self.addInput(anytype.AnyType, "value")
+        self.addOutput(das.types.TypeBase, "outDasObj")
+
+    def process(self):
+        dp = self.input("inDasObj").receive()
+        if dp.isEOP():
+            return False
+
+        dv = das.copy(dp.value())
+        dp.drop()
+
+        kp = self.input("key").receive()
+        if kp.isEOP():
+            return False
+
+        kv = kp.value()
+        kp.drop()
+
+        vp = self.input("value").receive()
+        if vp.isEOP():
+            return False
+
+        vv = vp.value()
+        vp.drop()
+
+        das.cli.set(dv, kv, str(vv))
+
+        self.output("outDasObj").send(dv)
+
+        return True
+
+
+class DasEval(block.Block):
+    def __init__(self):
+        super(DasEval, self).__init__()
+
+    def initialize(self):
+        self.addInput(das.types.TypeBase, "dasObj")
+        self.addInput(str, "expression")
+        self.addOutput(anytype.AnyType, "result")
+
+    def process(self):
+        dp = self.input("dasObj").receive()
+        if dp.isEOP():
+            return False
+
+        dv = dp.value()
+        dp.drop()
+
+        ep = self.input("expression").receive()
+        if ep.isEOP():
+            return False
+
+        ev = ep.value()
+        ep.drop()
+
+        self.output("result").send(das.cli.eval(dv, ev))
+
+        return True
+
+
+class DasAdd(block.Block):
+    def __init__(self):
+        super(DasAdd, self).__init__()
+
+    def initialize(self):
+        self.addInput(das.types.TypeBase, "inDasObj")
+        self.addInput(str, "key")
+        self.addInput(anytype.AnyType, "value")
+        self.addOutput(das.types.TypeBase, "outDasObj")
+
+    def process(self):
+        dp = self.input("inDasObj").receive()
+        if dp.isEOP():
+            return False
+
+        dv = das.copy(dp.value())
+        dp.drop()
+
+        kp = self.input("key").receive()
+        if kp.isEOP():
+            return False
+
+        kv = kp.value()
+        kp.drop()
+
+        vp = self.input("value").receive()
+        if vp.isEOP():
+            return False
+
+        vv = vp.value()
+        vp.drop()
+
+        das.cli.add(dv, kv, str(vv))
+        self.output("outDasObj").send(dv)
+
+        return True
